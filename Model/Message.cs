43df8df0c73a6b5188a0946e84676a4fd538b580
Amos Alagbe <sunboy005@gmail.com>
2022-02-21 16:48:31 +0100

    final-modifications

--- Model/Message.cs
+++ Model/Message.cs
@@ -5,11 +5,9 @@ namespace Models
     public class Message
     {
         public string Id { get; set; } = Guid.NewGuid().ToString();
-        public string CreatedDate { get; set; } = DateTime.Now.ToString();
         public string Name { get; init; }
         public string Email { get; init; }
         public string MessageText { get; init; }
-        public string FormName { get; set; }
-        public string FormDomainName { get; set; }
+        public string CreatedDate { get; set; } = DateTime.Now.ToString();
     }
 }
